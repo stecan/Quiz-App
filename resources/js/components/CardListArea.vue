7de8f5ca579615bb7ddf6181ad595bca42dc03de
ytskmt14 <u.s.kutieehmn14@gmail.com>
2021-12-12 23:45:55 +0900

    add component to display cardlist

---
+++ resources/js/components/CardListArea.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-row>
+        <v-col cols="2" v-for="(card, idx) in cardList" :key="idx">
+            <user-info-card
+                :profileImagePath="card.profileImagePath"
+                :userName="card.userName"
+                :department="card.department"
+            />
+
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+import UserInfoCard from './UserInfoCard'
+export default {
+    props: {
+        cardList: {
+            type: Array,
+            default: []
+        }
+    },
+    components: {
+        UserInfoCard,
+    }
+}
+</script>
