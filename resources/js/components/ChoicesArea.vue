54d2f4322273795b5500a08826867742943d46ff
ytskmt14 <u.s.kutieehmn14@gmail.com>
2021-12-19 21:26:27 +0900

    modify question area

---
+++ resources/js/components/ChoicesArea.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <v-card-text>
+            <v-radio-group
+                v-model="selection"
+                hide-details
+            >
+                <v-radio
+                    v-for="choice in choices"
+                    :key="choice"
+                    :value="choice"
+                    :label="choice"
+                />
+            </v-radio-group>
+        </v-card-text>
+        <v-card-actions
+            class="d-flex justify-center"
+        >
+            <v-btn
+                color="deep-purple lighten-2"
+                text
+                @click="sendAnswer()"
+            >
+                回答を送信する
+            </v-btn>
+        </v-card-actions>
+    </div>
+</template>
+
+<script>
+const TextButton = () => import('./common/TextButton');
+export default {
+    components:{
+        TextButton,
+    },
+    props: {
+        choices: {
+            type: Array,
+        },
+    },
+    data() {
+        return {
+            selection: this.choices[0]
+        }
+    },
+    methods: {
+        sendAnswer: function() {
+            if (window.confirm("選択している回答：" + this.selection + "\n回答を送信します。\nよろしいですか？")) {
+                // TODO: 回答送信のAPIを叩く
+                console.log(this.selection + "が送信されました。");
+            }
+            return false;
+        }
+    }
+}
+</script>
---
+++ resources/js/components/PanelistsArea.vue
@@ -0,0 +1,44 @@
+<template>
+<div>
+    <div
+        v-if="panelists.length"
+    >
+        <v-avatar
+            class="ma-2"
+            v-for="(panelist, index) in panelists"
+            :key="index"
+            size="125"
+        >
+            <img 
+                :src="panelist.profileImagePath"
+            />
+        </v-avatar>
+    </div>
+    <div v-else>回答者が決定していません。</div>
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const IMAGE_DIR = '/images/';
+const NO_IMAGE = 'NoImage.png';
+const EXTENSION = '.png';
+// const fs = require('fs');
+
+export default {
+    props: {
+        panelists: {
+            type: Array,
+            required: false,
+        }
+    },
+    created() {
+        this.getPanelists();
+    },
+    methods: {
+
+    },
+}
+</script>
