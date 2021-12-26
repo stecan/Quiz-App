a37b73843125e9f215f78e67ebf0f3febe1cef99
健吾 前田 <kngmed0209@gmail.com>
2021-12-26 15:42:17 +0900

    Merge branch 'deploy' of https://github.com/stecan/Quiz-App into deploy

--- resources/js/pages/FollowerSelect.vue
+++ resources/js/pages/FollowerSelect.vue
@@ -2,14 +2,20 @@
   <div>
     <v-app-bar id="app-bar" absolute app>
       <v-spacer />
-      <v-btn class="mr-5" color="primary" outlined x-large @click="getFollowerList">再選択</v-btn>
+      <v-btn class="mr-5" color="primary" outlined x-large @click="getFollowerList"
+        >再選択</v-btn
+      >
       <v-btn to="/" color="red" outlined x-large>確定</v-btn>
     </v-app-bar>
     <v-main>
-      <v-container>
-        <v-row>
+      <v-container fluid>
+        <v-row id="innerContainer">
           <v-col cols="12" xs="12" sm="4" md="3" v-for="card in cardList" :key="card.id">
-            <user-info-card :profileImagePath="card.img_path" :userName="card.user_name" :department="card.department" />
+            <user-info-card
+              :profileImagePath="card.img_path"
+              :userName="card.user_name"
+              :department="card.department"
+            />
           </v-col>
         </v-row>
       </v-container>
@@ -22,15 +28,15 @@
 <script>
 import axios from "axios";
 //axios.defaults.baseURL = '/bingo2021';
-import UserInfoCard from '../components/UserInfoCard.vue';
+import UserInfoCard from "../components/UserInfoCard.vue";
 
-const IMAGE_DIR = './images/player/';
-const NO_IMAGE = 'NoImage.png';
-const EXTENSION = '.jpg';
+const IMAGE_DIR = "./images/player/";
+const NO_IMAGE = "NoImage.png";
+const EXTENSION = ".jpg";
 
 export default {
   components: {
-    UserInfoCard
+    UserInfoCard,
   },
   data() {
     return {
@@ -68,3 +74,9 @@ export default {
   },
 };
 </script>
+<style scoped>
+#innerContainer {
+  max-width: 1175px;
+  margin: auto;
+}
+</style>
