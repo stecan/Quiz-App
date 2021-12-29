b5cecce7fcc2459a933fac900d31eb68eb5b34dd
kn885AammG246@outlook.jp <kn885AammG246@outlook.jp>
2021-12-29 13:06:30 +0900

    手札にエフェクトを追加

--- resources/js/components/CardListArea.vue
+++ resources/js/components/CardListArea.vue
@@ -10,6 +10,8 @@
         />
         <v-card-title class="pt-1">{{ card.userName }}</v-card-title>
         <v-card-subtitle class="pb-1">{{ card.department }}</v-card-subtitle>
+        <div :class="checkStatus(card)"></div>
+        <img v-show="card.status == '1'" class="medal" src="https://frame-illust.com/fi/wp-content/uploads/2016/04/6903.png" />
       </v-card>
     </div>
   </Slick>
@@ -82,6 +84,68 @@ export default {
         this.$refs.slick.reSlick();
       });
     },
+    // マーク表示
+    checkStatus: function(card) {
+      var cssClass = 'frame';
+      if (card.kbn == '2')
+      {
+        cssClass += ' spot'; // 回答中
+      }
+      else if (card.kbn == '9')
+      {
+        // 回答済み
+        cssClass += ' fix';
+      }
+      else
+      {
+        // 未回答
+        cssClass += ' pre';
+      }
+      return cssClass;
+    },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+.frame {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 99;
+}
+
+.medal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 30px;
+    height: 30px;
+    z-index: 100;
+}
+
+@keyframes spot-ani{
+    0%   { opacity:0; }
+    100% { opacity:1; }
+}
+@-webkit-keyframes spot-ani{
+    0%   { opacity:0; }
+    100% { opacity:1; }
+}
+
+.spot {
+  border: 3px violet solid !important;
+  animation:spot-ani ease-in-out 1s infinite forwards;
+  -webkit-animation:spot-ani ease-in-out 1s infinite alternate;
+}
+.pre {
+  border: 3px rgba(145, 224, 145, 0.685) solid !important;
+}
+.fix {
+  border: 3px gray solid !important;
+}
+
+</style>
